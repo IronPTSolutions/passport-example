9cc8f8b4599d21a7a9a88f51d38d6a0b39f28475
Mercedes Jurado Pérez <merjuradoperez@gmail.com>
2022-08-20 00:06:15 +0200

    1st commit

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,14 +1,31 @@
 const express = require('express');
-
 const router = express.Router();
-
+const passport = require("passport");
 const authController = require('../controllers/auth.controller');
+const userController = require ("../controllers/user.controller");
+const authMiddlewares = require("../middlewares/authMiddleware");
+
+const SCOPES = ["profile", "email"];
 
 router.get('/', (req, res, next) => {
   res.render('index')
 })
 
-router.get('/register', authController.register)
-router.get('/login', authController.login)
+// authentication
+router.get('/register', authController.register);
+router.post("register", authController.doRegister);
+router.get('/login', authMiddlewares.isNotAuthenticated, authController.login);
+router.post("/login", authController.doLogin);
+
+//passport
+router.get("/login/google", authMiddlewares.isNotAuthenticated, passport.authenticate("google-auth", { scope: SCOPES }));
+router.get("/auth/google/callback", authMiddlewares.isNotAuthenticated, authController.doLoginGoogle);
+router.get("/logout", authMiddlewares.isAuthenticated, authController.logout);
+
+//users
+router.get("/profile", authMiddlewares.isAuthenticated, userController.profile);
+
+  
+
 
 module.exports = router;
