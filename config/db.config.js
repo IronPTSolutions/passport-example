c48721b0b178d3f75cb549ccad17387d948ae91b
mblason <marina.blasongraviz@gmail.com>
2022-08-23 13:24:28 +0200

    lab done

--- config/db.config.js
+++ config/db.config.js
@@ -1,17 +1,25 @@
-const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/passport-example';
+const mongoose = require("mongoose");
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/iron-store";
 
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
-  .catch((error) => {
-    console.error(`An error ocurred trying to connect to de database ${MONGODB_URI}`, error);
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(`Connected to db at ${MONGODB_URI}`);
+  })
+  .catch((err) => {
+    console.error(`Error connecting to ${MONGODB_URI}`, err);
+
     process.exit(0);
   });
 
-process.on('SIGINT', function () {
+process.on("SIGINT", function () {
   mongoose.connection.close(function () {
-    console.log('Mongoose disconnected on app termination');
+    console.log("Mongoose disconnected on app termination");
     process.exit(0);
   });
 });
--- models/User.model.js
+++ models/User.model.js
@@ -1,29 +1,72 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const EMAIL_PATTERN = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const EMAIL_PATTERN =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 const PASSWORD_PATTERN = /^.{8,}$/i;
 const SALT_ROUNDS = 10;
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: 'name is required',
-    minLength: [3, 'name needs at least 3 chars']
+    required: [true, "Name is required"],
+    minLength: [3, "Name must contain at least 3 characters"],
   },
   email: {
     type: String,
-    required: 'email is required',
-    match: [EMAIL_PATTERN, 'email is not valid'],
-    unique: true
+    required: [true, "Email is required"],
+    unique: true,
+    match: [EMAIL_PATTERN, "Email pattern does not match"],
   },
   password: {
     type: String,
-    required: 'password is required',
-    match: [PASSWORD_PATTERN, 'password needs at least 8 chars'],
+    required: [true, "Password is required"],
+    match: [PASSWORD_PATTERN, "Password must contain 8 characters"],
   },
+  googleID: {
+    type: String
+  },
+  image: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/plasoironhack/image/upload/v1644663323/ironhack/multer-example/icono-de-li%CC%81nea-perfil-usuario-si%CC%81mbolo-empleado-avatar-web-y-disen%CC%83o-ilustracio%CC%81n-signo-aislado-en-fondo-blanco-192379539_jvh06m.jpg",
+  },
+  status: {
+    type: Boolean,
+    default: false
+  },
+  activationToken: {
+    type: String,
+    default: () => {
+      return Math.random().toString(36).substring(7) +
+      Math.random().toString(36).substring(7) +
+      Math.random().toString(36).substring(7) +
+      Math.random().toString(36).substring(7)
+    }
+  }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    bcrypt
+      .hash(user.password, SALT_ROUNDS)
+      .then((hash) => {
+        user.password = hash;
+        next();
+      })
+      .catch((err) => next(err));
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.checkPassword = function (password) {
+  const user = this;
+  return bcrypt.compare(password, user.password);
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
