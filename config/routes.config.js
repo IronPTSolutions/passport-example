042f1a33a114f59f1ddd5e10159e4970e7aa1bca
silvi-hdez <silviahdez.b@gmail.com>
2023-02-06 14:24:57 +0100

    primeros pasos. Tengo que modificar el <nav> para poder hacer logout

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const { authorize } = require('passport');
 
 const router = express.Router();
 
 const authController = require('../controllers/auth.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
+const userController = require('../controllers/user.controller')
 
 router.get('/', (req, res, next) => {
   res.render('index')
 })
 
-router.get('/register', authController.register)
-router.get('/login', authController.login)
+router.get('/register', authMiddleware.isNotAuthenticated,authController.register)
+router.post('/register', authMiddleware.isNotAuthenticated, authController.doRegister)
+
+router.get('/login', authMiddleware.isNotAuthenticated, authController.login)
+router.post('/login', authMiddleware.isNotAuthenticated, authController.doLogin)
+
+router.get('logout', authMiddleware.isAuthenticated, authController.logOut)
+
+router.get('/profile', authMiddleware.isAuthenticated, userController.profile)
 
 module.exports = router;
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,60 @@
+const mongoose = require('mongoose')
+const User = require('../models/User.model')
+const passport = require('passport')
+
 module.exports.register = (req, res, next) => {
   res.render('auth/register', )
 }
 
+module.exports.doRegister = (req, res, next) => {
+  const user = req.body
+
+  const renderWithErrors = (errors) => {
+    res.render('auth/register', {errors, user})
+  }
+
+  User.findOne({email: user.email})
+  .then((isUser) => {
+    if(isUser) {renderWithErrors, {email: 'Email already in use'}
+    } else {
+      return User.create(user)
+        .then(() => {
+          res.redirect ('/login')
+        })
+    }
+  })
+  .catch(err => {
+    if(err instanceof mongoose.Error.ValidationError) {
+      renderWithErrors(err.error)
+    }else {
+      next(err)
+    }
+  })
+}
+
 module.exports.login = (req, res, next) => {
   res.render('auth/login', )
+}
+
+module.exports.doLogin = (req, res, next) => {
+ passport.authenticate('local-auth', (err, user, validations) => {
+  if(err) {
+    next(err)
+  } else if (!user) {
+    res.status(404).render('auth/login', {user: req.body, errorMessage: validations.error})
+  } else {
+    req.login(user, (loginErr) => {
+      if (!loginErr) {
+        next(loginErr)
+      } else {
+        res.redirect('/profile')
+      }
+    })
+  }
+ }) (req, res, next)
+}
+
+module.exports.logOut = (req, res, next) => {
+  req.logOut();
+  res.redirect('/login')
 }
