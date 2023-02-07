886ec83001ff9f1e4385bbdbcb8492b6fb09872c
pianattero <mariapianattero@gmail.com>
2023-02-07 19:44:39 +0100

    feat: register&login with Google

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,15 +1,14 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-const GOOGLE_SCOPES = [
-  "https://www.googleapis.com/auth/userinfo.profile",
-  "https://www.googleapis.com/auth/userinfo.email"
-]
-
 const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
+const GOOGLE_SCOPES = [
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/userinfo.email"
+]
 
 router.get('/', (req, res, next) => {
   res.render('index')
@@ -20,6 +19,9 @@ router.post('/register', authMiddleware.isNotAuthenticated, authController.doReg
 
 router.get('/login', authMiddleware.isNotAuthenticated, authController.login);
 router.post('/login', authMiddleware.isNotAuthenticated, authController.doLogin);
+
+router.get('/login/google', authMiddleware.isNotAuthenticated, passport.authenticate('google-auth', { scope: GOOGLE_SCOPES }));
+router.get('/auth/google/callback', authMiddleware.isNotAuthenticated, authController.doLoginGoogle);
 
 router.get('/logout', authMiddleware.isAuthenticated, authController.doLogout);
 
