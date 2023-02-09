5d1b13502c4d5066f665541892f8ea345b28a157
Manuel Da Silva <ktireds2803@gmail.com>
2023-02-09 18:46:50 +0100

    Refactoring code

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,14 +1,39 @@
-const express = require('express');
-
+const express = require("express");
+const passport = require("passport");
 const router = express.Router();
+const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
-const authController = require('../controllers/auth.controller');
+const GOOGLE_SCOPES = [
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/userinfo.email",
+];
 
-router.get('/', (req, res, next) => {
-  res.render('index')
-})
-
-router.get('/register', authController.register)
-router.get('/login', authController.login)
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+router.get(
+  "/register",
+  authMiddleware.isNotAuthenticated,
+  authController.register
+);
+router.get("/login", authMiddleware.isNotAuthenticated, authController.login);
+router.post(
+  "/register",
+  authMiddleware.isNotAuthenticated,
+  authController.doRegister
+);
+router.post(
+  "/login",
+  authMiddleware.isNotAuthenticated,
+  authController.doLogin
+);
+router.get("/profile", authMiddleware.isAuthenticated, authController.profile);
+router.get(
+  "/login/google",
+  passport.authenticate("google-auth", { scope: GOOGLE_SCOPES })
+);
+router.get("/auth/google/callback", authController.doLoginGoogle);
+router.get("/logout", authController.doLogout);
 
 module.exports = router;
--- models/User.model.js
+++ models/User.model.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
 const EMAIL_PATTERN = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 const PASSWORD_PATTERN = /^.{8,}$/i;
 const SALT_ROUNDS = 10;
-
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: 'name is required',
@@ -22,8 +20,26 @@ const userSchema = new Schema({
     required: 'password is required',
     match: [PASSWORD_PATTERN, 'password needs at least 8 chars'],
   },
+  googleID: {
+    type: String
+  },
 });
 
+userSchema.pre('save', function (next) {
+  const rawPassword = this.password;
+  if (this.isModified('password')) {   
+    bcrypt.hash(rawPassword, SALT_ROUNDS)
+      .then(hash => {
+        this.password = hash;
+        next()
+      })
+      .catch(err => next(err))
+  } else {
+    next();
+  }
+});
+userSchema.methods.checkPassword = function (passwordToCompare) {
+  return bcrypt.compare(passwordToCompare, this.password);
+}
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
